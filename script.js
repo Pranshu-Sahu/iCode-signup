404c910e885a1eba838006daec9319fa16da0c75
Pranshu <sahupranshu637@gmail.com>
2022-12-24 16:48:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// grab sign up form
+const signUpForm = document.getElementById("sign-up-form");
+document.getElementById("close-btn").addEventListener("click", () => {
+  signUpForm.remove();
+});
